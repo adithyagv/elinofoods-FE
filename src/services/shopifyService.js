56c1146afc7b0db154ebbdb657e3ae32ef04f6f5
adith3645 <adithya.gv@innovites.com>
2025-08-10 23:54:19 +0530

    Added deployed backend link

--- src/services/shopifyService.js
+++ src/services/shopifyService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  "https://elinofoods-be.onrender.com/api" || "http://localhost:5000/api";
 
 // Add axios interceptor for better debugging
 axios.interceptors.request.use(
